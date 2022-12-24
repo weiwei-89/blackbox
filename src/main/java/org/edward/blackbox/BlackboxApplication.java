486a523c0aa198dcd901652cb8e65ea7b25b4a2b
Edward_89 <wangwei.891126@gmail.com>
2022-12-25 01:15:22 +0800

    添加SpringBoot依赖

---
+++ src/main/java/org/edward/blackbox/BlackboxApplication.java
@@ -0,0 +1,11 @@
+package org.edward.blackbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackboxApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlackboxApplication.class, args);
+    }
+}
